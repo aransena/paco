427a248596edead2fc4ce6f21efcf49b590cc1bf
aransena <senaa@tcd.ie>
2015-12-14 17:29:11 +0000

    circular and randomly distributed worlds

--- simulation_main.py
+++ simulation_main.py
@@ -1,25 +1,35 @@
 #!/usr/bin/env python
 import city
 import world
+from random import randint
+import math
 
-dublin = city.City(0,10,10)
-london = city.City(1,30,30)
-italy= city.City(2,14,14)
+NUM_CITIES=100
+WORLD_X = 100
+WORLD_Y = 100
 
 earth = world.World()
 
-earth.add_cities(dublin)
-earth.add_cities(london)
-earth.add_cities(italy)
+mode = 0 #Mode 0: Uninform random Distribution, Mode 1: Circle
 
+if mode==1:
+    increment = 360.0/NUM_CITIES
+
+for i in range(0,NUM_CITIES):
+    if mode == 0:
+        earth.add_cities(city.City(i,randint(0,WORLD_X),randint(0,WORLD_Y)))
+        
+    elif mode == 1:
+        angle = i*increment
+        earth.add_cities(city.City(i,math.sin(math.radians(angle)),math.cos(math.radians(angle))))
+
+
+# Visualize world
 import matplotlib.pyplot as plt
-
-print "CITIES: ", earth.cities
-print "DUBLIN x: ", dublin.x
-
 plt.figure()
+plt.autoscale(tight=False)
+plt.margins(0.1,0.1)
 for c in earth.cities:
-    print c
     plt.plot(c.x, c.y,'ro')
 plt.show()
     
