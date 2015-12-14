3a0c40c8d49fc145db2f9f0c55c144f869c30b95
aransena <senaa@tcd.ie>
2015-12-14 17:46:44 +0000

    bizarro world added

--- city.py
+++ city.py
@@ -1,13 +1,15 @@
 #!/usr/bin/env python
 
 class City:
-    def __init__(self,i, x_cord, y_cord):
+    def __init__(self,i, x_cord, y_cord,prob):
         self.index = i
         self.x = x_cord
         self.y = y_cord
         self.objects = []
-
+        self.probability = prob
+        
     def add_objects(hidden_objects):
         self.objects.append(hidden_objects)
+    
         
         
--- simulation_main.py
+++ simulation_main.py
@@ -1,35 +1,63 @@
 #!/usr/bin/env python
 import city
 import world
-from random import randint
+import random
 import math
 
-NUM_CITIES=100
+random.seed(1)
+
+NUM_CITIES=10
 WORLD_X = 100
 WORLD_Y = 100
 
 earth = world.World()
+bizarro = world.World()
 
-mode = 0 #Mode 0: Uninform random Distribution, Mode 1: Circle
+mode = 1 #Mode 0: Uninform random Distribution, Mode 1: Circle
 
 if mode==1:
     increment = 360.0/NUM_CITIES
 
 for i in range(0,NUM_CITIES):
+
+    prob = random.random()
+    
     if mode == 0:
-        earth.add_cities(city.City(i,randint(0,WORLD_X),randint(0,WORLD_Y)))
+        earth.add_cities(city.City(i,random.uniform(0,WORLD_X),random.uniform(0,WORLD_Y),prob))
         
     elif mode == 1:
         angle = i*increment
-        earth.add_cities(city.City(i,math.sin(math.radians(angle)),math.cos(math.radians(angle))))
+        earth.add_cities(city.City(i,math.sin(math.radians(angle)),math.cos(math.radians(angle)),prob))
 
+for i, c in enumerate(earth.cities):
+    bizarro.add_cities(city.City(i, (c.x)/c.probability, (c.y)/c.probability,1))
+            
+        
 
 # Visualize world
+
 import matplotlib.pyplot as plt
 plt.figure()
 plt.autoscale(tight=False)
+
+plt.subplot(211)
 plt.margins(0.1,0.1)
-for c in earth.cities:
-    plt.plot(c.x, c.y,'ro')
+for i,c in enumerate(earth.cities):
+    if i == 0:
+        plt.plot(c.x, c.y,'go')
+    elif i == len(earth.cities)-1:
+        plt.plot(c.x, c.y,'ro')
+    else:
+        plt.plot(c.x, c.y,'bo')
+plt.subplot(212)
+plt.margins(0.1,0.1)
+for i,c in enumerate(bizarro.cities):
+    if i == 0:
+        plt.plot(c.x, c.y,'go')
+    elif i == len(earth.cities)-1:
+        plt.plot(c.x, c.y,'ro')
+    else:
+        plt.plot(c.x, c.y,'bo')
+                       
 plt.show()
     
