91c09eb7e2fc5eef16ba2b6b2a3d07ec48a0146d
aransena <senaa@tcd.ie>
2016-01-05 18:48:26 +0000

    updates...

--- paco_example.py
+++ paco_example.py
@@ -11,9 +11,9 @@ increment = 360.0/num_cities
 aco = paco.ACO(num_cities,initial_pheromone=1,alpha=1,beta=3,epsilon=0.1,pheromone_deposit=2,evaporation_constant=0.6)
 
 for i in range(0, num_cities):
-    aco.add_cities(paco.City(i, random.uniform(0,world_x), random.uniform(0,world_y)))
-    #angle = i*increment
-    #aco.add_cities(paco.City(i,math.sin(math.radians(angle)),math.cos(math.radians(angle))))
+    #aco.add_cities(paco.City(i, random.uniform(0,world_x), random.uniform(0,world_y)))
+    angle = i*increment
+    aco.add_cities(paco.City(i,math.sin(math.radians(angle)),math.cos(math.radians(angle))))
     
 shortest_path = aco.get_best_path(num_ants=3,num_steps=10)
 print "Shortest route found: ", aco.shortest_path_len
